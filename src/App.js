527c9be7cff777ecb6e43a7a73acc2053af06708
kkebuka <kkebuka@gmail.com>
2024-07-04 21:00:27 +0100

    updated App file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Banner from "./components/Banner";
+import BestProducts from "./components/BestProducts";
 import Navbar from "./components/Navbar";
 import TopProduct from "./components/TopProduct";
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Banner />
       <TopProduct />
+      <BestProducts />
     </>
   );
 }
