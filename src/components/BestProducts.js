ef873a8c3769a8555696c3f54a49c66f336af572
kkebuka <kkebuka@gmail.com>
2024-07-04 21:02:04 +0100

    BestProducts file added

---
+++ src/components/BestProducts.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BestProducts() {
+  return (
+    <div>
+      <strong>Best Products</strong>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        molestiae illo nihil!
+      </p>
+    </div>
+  );
+}
+
+export default BestProducts;
