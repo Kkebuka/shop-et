08c3c97b9886e5098226747d9379fe1d43f890b2
kkebuka <kkebuka@gmail.com>
2024-07-04 21:03:20 +0100

    top products

--- src/components/TopProduct.js
+++ src/components/TopProduct.js
@@ -3,14 +3,15 @@ import TopGridView from "./TopGridView";
 
 function TopProduct() {
   return (
-    <div>
-      <h2>Top Selling Products for you</h2>
-      <strong>Products</strong>
-      <p>
+    <div className="flex flex-col items-center py-12">
+      <h2 className="text-md">Top Selling Products for you</h2>
+      <strong className="text-4xl">Products</strong>
+      <p className="text-md">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est amet
         ducimus non!
       </p>
       <TopGridView />
+      <button className="mt-12 p-4">View All Products</button>
     </div>
   );
 }
