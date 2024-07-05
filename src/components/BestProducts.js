460cddc64507cad4648df49ffc1e003a424971a7
kkebuka <kkebuka@gmail.com>
2024-07-05 21:50:52 +0100

    update

--- src/components/BestProducts.js
+++ src/components/BestProducts.js
@@ -1,13 +1,18 @@
 import React from "react";
+import BestGridView from "./BestGridView";
 
 function BestProducts() {
   return (
     <div>
-      <strong>Best Products</strong>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-        molestiae illo nihil!
-      </p>
+      <div className="flex flex-col items-center">
+        <strong>Best Products</strong>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+          molestiae illo nihil!
+        </p>
+      </div>
+
+      <BestGridView />
     </div>
   );
 }
