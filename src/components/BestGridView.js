2c5440b57c13f194b29eda363c13b33080b9e56e
kkebuka <kkebuka@gmail.com>
2024-07-05 21:51:44 +0100

    minor update

---
+++ src/components/BestGridView.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function BestGridView() {
+  return (
+    <div>
+      <div className=" h-[300px] flex flex-col shadow-lg ">
+        <img src="" alt="" />
+        {/* Space for rating star */}
+        <strong className="text-center">Casual Wear</strong>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          corporis?
+        </p>
+        <button>Order Now</button>
+      </div>
+    </div>
+  );
+}
+
+export default BestGridView;
