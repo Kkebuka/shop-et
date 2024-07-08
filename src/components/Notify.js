1c10e945c6a55cdb266b55bf8ebecba55b41300e
kkebuka <kkebuka@gmail.com>
2024-07-08 14:55:15 +0100

    notify file

---
+++ src/components/Notify.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notify() {
+  return (
+    <div className=" flex flex-col items-center border-2 h-[200px] justify-center">
+      <strong>Get Notified About New Products</strong>
+      <input type="email" className="" />
+    </div>
+  );
+}
+
+export default Notify;
