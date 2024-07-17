532fcf72adaa35676c8f7df82f77e3ac35b372ff
kkebuka <kkebuka@gmail.com>
2024-07-17 13:25:18 +0100

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "button-color": "#F99F1C",
+        "background-color": "#FBB637",
+      },
+    },
   },
   plugins: [],
 };
